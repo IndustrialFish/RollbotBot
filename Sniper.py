57d127e4da9e316b6bd6beb97842b5775facd928
IndustrialFish <97189368+IndustrialFish@users.noreply.github.com>
2022-04-20 11:55:16 +0100

    Built in basic robustness

--- Sniper.py
+++ Sniper.py
@@ -10,28 +10,36 @@ time.sleep(60)
 
 while (True):
 
-    time.sleep(5)
-
-    driver.find_element(By.XPATH, '/html/body/div[1]/div[6]/div[1]/div/div[3]/button[2]').click()  # Click Main Filter
-    driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div/div[3]').click()  # Click on Lowest Price Filter
-
-    time.sleep(2)
-
-    price = driver.find_element(By.XPATH, '//*[@id="root"]/div[6]/div[1]/div/div[4]/div[1]/div[4]/div[3]/div').text.replace("$", "")
-    # Price of the cheapest NFT
-    print(price)
-
     try:
-        priceFloat = float(price)
+        time.sleep(5)
+
+        driver.find_element(By.XPATH,
+                            '/html/body/div[1]/div[6]/div[1]/div/div[3]/button[2]').click()  # Click Main Filter
+        driver.find_element(By.XPATH, '/html/body/div[2]/div/div/div/div[3]').click()  # Click on Lowest Price Filter
+
+        time.sleep(5)
+
+        price = driver.find_element(By.XPATH,
+                                    '//*[@id="root"]/div[6]/div[1]/div/div[4]/div[1]/div[4]/div[3]/div').text.replace(
+            "$", "")
+        # Price of the cheapest NFT
+        print(price)
+
+        try:
+            priceFloat = float(price)  # Convert price to float in order to compare numerically.
+
+        except:
+            priceFloat = 1000.0  # Set the price over the buy price if can't read.
+
+        buyPrice = 850  # Price at which you want to buy bots under.
+
+        if priceFloat < buyPrice:
+            driver.find_element(By.XPATH, '//*[@id="root"]/div[6]/div[1]/div/div[4]/div[1]/div[5]/button[1]').click()
+            # Click on Lowest Price Filter
+            print("BUY | " + driver.find_element(By.XPATH,
+                                                 '//*[@id="root"]/div[6]/div[1]/div/div[4]/div[1]/div[1]/div/a/div').text + " | Price: $" + price)
+
+        driver.refresh()  # Refresh browser window.
 
     except:
-        priceFloat = 1000.0
-
-    buyPrice = 850
-
-    if priceFloat < buyPrice:
-        driver.find_element(By.XPATH, '//*[@id="root"]/div[6]/div[1]/div/div[4]/div[1]/div[5]/button[1]').click()
-        # Click on Lowest Price Filter
-        print("BUY | " + driver.find_element(By.XPATH, '//*[@id="root"]/div[6]/div[1]/div/div[4]/div[1]/div[1]/div/a/div').text + " | Price: $" + price)
-
-    driver.refresh()
+        None
